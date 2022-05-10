19795c2b2b198babe1731084541ca23ba10ea48a
Łukasz Kaczmarek <luka.kaczmarek@gmail.com>
2022-05-10 11:51:03 +0100

    before adding swiper

---
+++ src/components/collections/product/ProductDetailsAdmin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+  <add-image :product="product"></add-image>
+</template>
+
+<script>
+export default {
+  props: ['product'],
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
