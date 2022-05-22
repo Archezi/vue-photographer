088227a805455701f6385e4d984b58c7f0a85dd0
Łukasz Kaczmarek <luka.kaczmarek@gmail.com>
2022-05-22 13:21:07 +0100

    creating swiper component

---
+++ src/components/swiper/swiper.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="slider-wrapper">
+    <swiper
+      :slidesPerView="'1.7'"
+      :centeredSlides="true"
+      :spaceBetween="0"
+      :loop="true"
+      :navigation="true"
+      :pagination="{
+        clickable: true
+      }"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide v-for="(slide, index) in slider" :key="index">
+        <router-link :to="{ name: 'Products' }">
+          <img :src="slide.imageUrl" alt="" />
+        </router-link>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper } from 'swiper/vue/swiper'
+import { SwiperSlide } from 'swiper/vue/swiper-slide'
+// Import Swiper styles
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/modules/navigation/navigation'
+// import required modules
+import { Navigation } from 'swiper'
+export default {
+  props: ['slider'],
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  setup() {
+    return {
+      modules: [Navigation]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.slider-wrapper {
+  position: relative;
+  height: var(--swiper-height);
+}
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+
+  /* Center slide text vertically */
+}
+.swiper-slide a {
+  width: 100%;
+  // height: 100%;
+}
+.swiper-slide img {
+  opacity: 0.8;
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: all 0.5s ease-in-out;
+  // filter: blur(2px);
+}
+.swiper-slide-active img {
+  opacity: 1;
+  // filter: blur(0px);
+}
+.swiper-slide {
+  width: 60%;
+}
+
+.swiper-slide:nth-child(2n) {
+  width: 40%;
+}
+
+.swiper-slide:nth-child(3n) {
+  width: 20%;
+}
+.swiper-button-next {
+  height: 100% !important;
+  top: 46.5% !important;
+  transform: translateY(-50%);
+}
+.swiper-button-next:focus-visible {
+  outline: none;
+}
+</style>
